c2f6e6a8d135b9942f399ac994ad68dc83ff265f
juliana <julianatelles.contato@gmail.com>
2024-06-02 21:12:23 -0300

    TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -21,8 +21,7 @@ namespace DesafioProjetoHospedagem.Models
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+               throw new Exception("Capacidade máxima de hóspedes excedida");
             }
         }
 
